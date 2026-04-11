e69c8852633babe93bd677b9641a2962f346043d
ravi <raviksann@outlook.com>
2026-04-11 19:25:32 +0200

    fish config

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,39 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
-  programs.fish.enable = true;
+programs.fish = {
+  enable = true;
+  interactiveShellInit = ''
+    # Tomorrow Night Bright color scheme
+    set -g fish_color_autosuggestion 969896
+    set -g fish_color_cancel --reverse
+    set -g fish_color_command c397d8
+    set -g fish_color_comment e7c547
+    set -g fish_color_cwd green
+    set -g fish_color_cwd_root red
+    set -g fish_color_end c397d8
+    set -g fish_color_error d54e53
+    set -g fish_color_escape 00a6b2
+    set -g fish_color_history_current --bold
+    set -g fish_color_host normal
+    set -g fish_color_host_remote yellow
+    set -g fish_color_normal normal
+    set -g fish_color_operator 00a6b2
+    set -g fish_color_param 7aa6da
+    set -g fish_color_quote b9ca4a
+    set -g fish_color_redirection 70c0b1
+    set -g fish_color_search_match white --bold --background=brblack
+    set -g fish_color_selection white --bold --background=brblack
+    set -g fish_color_status red
+    set -g fish_color_user brgreen
+    set -g fish_color_valid_path --underline=single
+    set -g fish_pager_color_completion normal
+    set -g fish_pager_color_description B3A06D
+    set -g fish_pager_color_prefix normal --bold --underline=single
+    set -g fish_pager_color_progress brwhite --bold --background=cyan
+    set -g fish_pager_color_selected_background --background=brblack
+  '';
+};
   programs.firefox.enable = true;
 
   nixpkgs.config.allowUnfree = true;
